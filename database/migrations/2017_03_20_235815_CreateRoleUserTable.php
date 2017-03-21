8a5d4a09ce2aaac15294ec3e0ec81a505298eb0d
Yury Firsanau <rokki@tut.by>
2017-03-21 03:18:14 +0200

    edit migration RoleUserTable

--- database/migrations/2017_03_20_235815_CreateRoleUserTable.php
+++ database/migrations/2017_03_20_235815_CreateRoleUserTable.php
@@ -12,8 +12,9 @@ class CreateRoleUserTable extends Migration
      */
     public function up()
     {
-        Schema::create('user_role', function (Blueprint $table) {
+        Schema::create('role_user', function (Blueprint $table) {
             $table->increments('id');
+
             $table->timestamps();
         });
     }
@@ -25,6 +26,6 @@ class CreateRoleUserTable extends Migration
      */
     public function down()
     {
-        Schema::drop('user_role');
+        Schema::drop('role_user');
     }
 }
